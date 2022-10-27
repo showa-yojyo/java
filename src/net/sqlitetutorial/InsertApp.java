3a8995109a9692582148925e66598e12b831b3ac
showa_yojyo <yojyo@hotmail.com>
2022-10-27 23:18:13 +0900

    src/InsertApp.java: insertion demo

---
+++ src/net/sqlitetutorial/InsertApp.java
@@ -0,0 +1,49 @@
+package net.sqlitetutorial;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author sqlitetutorial.net
+ */
+public class InsertApp extends AppBase{
+    /**
+     * @param url SQLite connection string
+     */
+    public InsertApp(String url) {
+        super(url);
+    }
+
+    /**
+     * Insert a new row into the warehouses table
+     *
+     * @param name name of the warehouse
+     * @param capacity capacity of the warehouse
+     */
+    public void insert(String name, double capacity) {
+        String sql = "INSERT INTO warehouses(name,capacity) VALUES(?,?)";
+
+        try (Connection conn = this.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setDouble(2, capacity);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // "jdbc:sqlite:" + "../../test.db"
+        InsertApp app = new InsertApp("jdbc:sqlite:" + args[0]);
+        // insert three new rows
+        app.insert("Raw Materials", 3000);
+        app.insert("Semifinished Goods", 4000);
+        app.insert("Finished Goods", 5000);
+    }
+}
